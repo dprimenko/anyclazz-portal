59c59b1fe1effff4b2b12cd71c56cee94b1e93b1
David Primenko <davidprimenko@gmail.com>
2026-02-18 16:43:50 +0100

    wip

--- src/ui-library/components/form/text-area/Textarea.tsx
+++ src/ui-library/components/form/text-area/Textarea.tsx
@@ -2,7 +2,7 @@ import cn from "classnames";
 
 export function Textarea({ value, onChange, placeholder, rows, className }: { value?: string; onChange: (value: string) => void; placeholder?: string; rows?: number; className?: string }) {
     const classNames = cn(
-        "w-full px-4 py-3 rounded-lg border-2 border-[var(--color-neutral-300)] bg-white text-[var(--color-neutral-900)] placeholder:text-[var(--color-neutral-500)] focus:outline-none focus:border-[var(--color-primary-700)] transition-colors resize-none",
+        "w-full px-4 py-3 rounded-lg border-1 border-[var(--color-neutral-300)] bg-white text-[var(--color-neutral-900)] placeholder:text-[var(--color-neutral-500)] focus:outline-none focus:border-2 focus:border-[var(--color-primary-700)] transition-colors resize-none",
         className
     );
 
