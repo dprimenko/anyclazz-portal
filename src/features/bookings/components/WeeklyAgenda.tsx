fb2172be994a064fa88997f69c5bfccc59dc42fb
David Primenko <davidprimenko@gmail.com>
2026-04-12 15:36:37 +0200

    wip

--- src/features/bookings/components/WeeklyAgenda.tsx
+++ src/features/bookings/components/WeeklyAgenda.tsx
@@ -183,6 +183,7 @@ export function WeeklyAgenda({ bookings: initialBookings, user, token, lang, ini
                             colorType="primary"
                             fullWidth
                         />
+                    </a>
                 </div>
             </div>
 
